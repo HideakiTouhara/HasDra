0ff3421ec9ea800068361a6c45f12f1aeb16df88
HideakiTouhara <hideee6723@gmail.com>
2017-07-07 01:51:29 +0900

    Create GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+
+	public const int MachingCount = 3;
+
+	enum GameState
+	{
+		Idle,
+		PieceMove,
+		DeletePiece,
+		FillPiece,
+	}
+
+	[SerializeField]
+	private Board board;
+	[SerializeField]
+	private Text stateText;
+
+	private GameState currentState;
+	private Piece selectedPiece;
+
+	// Use this for initialization
+	void Start () 
+	{	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
